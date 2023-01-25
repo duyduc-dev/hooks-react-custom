806835135c2c4a183c17e552c40c4f8926b004da
duyduc2003 <dangduyducdangduyduc@gmail.com>
2023-01-25 17:37:05 +0700

    update new hook

--- src/hooks/useAsync.ts
+++ src/hooks/useAsync.ts
@@ -1,37 +1,65 @@
 import { useCallback, useEffect, useState } from 'react'
 
+export interface OptionsAsync {
+  immediate?: boolean
+  log?: boolean
+}
+
+export type StatusModal = 'idle' | 'pending' | 'success' | 'error'
+
+export interface UseAsyncState<T, E = string> {
+  status: StatusModal
+  value: T | null
+  error: E | null
+}
+
 // Hook
-const useAsync = <T, E = string>(asyncFunction: () => Promise<T>, immediate = true) => {
-  const [status, setStatus] = useState<'idle' | 'pending' | 'success' | 'error'>('idle')
-  const [value, setValue] = useState<T | null>(null)
-  const [error, setError] = useState<E | null>(null)
+const useAsync = <T, E = string>(asyncFunction: () => Promise<T>, options?: OptionsAsync) => {
+  const [state, setState] = useState<UseAsyncState<T, E>>({
+    status: 'idle',
+    value: null,
+    error: null,
+  })
+
   // The execute function wraps asyncFunction and
   // handles setting state for pending, value, and error.
   // useCallback ensures the below useEffect is not called
   // on every render, but only if asyncFunction changes.
   const execute = useCallback(() => {
-    setStatus('pending')
-    setValue(null)
-    setError(null)
+    setState({
+      status: 'pending',
+      value: null,
+      error: null,
+    })
     return asyncFunction()
       .then((response: any) => {
-        setValue(response)
-        setStatus('success')
+        setState({
+          status: 'success',
+          value: response,
+          error: null,
+        })
       })
       .catch((error: any) => {
-        setError(error)
-        setStatus('error')
+        setState({
+          status: 'error',
+          value: null,
+          error: error,
+        })
       })
   }, [asyncFunction])
   // Call execute if we want to fire it right away.
   // Otherwise execute can be called later, such as
   // in an onClick handler.
   useEffect(() => {
-    if (immediate) {
+    if (options?.immediate) {
       execute()
     }
-  }, [execute, immediate])
-  return { execute, status, value, error }
+  }, [execute, options?.immediate])
+
+  useEffect(() => {
+    if (options?.log) console.log(state)
+  }, [options?.log, state])
+  return { execute, ...state }
 }
 
 export { useAsync }
