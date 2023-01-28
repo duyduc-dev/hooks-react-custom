de353e27c00249bbfa553f1e5ce5d322b7b52230
duyduc2003 <dangduyducdangduyduc@gmail.com>
2023-01-28 14:00:10 +0700

    update useScrollToElement

--- src/hooks/useScrollToElement.ts
+++ src/hooks/useScrollToElement.ts
@@ -4,7 +4,7 @@ type UseScrollToElementReturn<T> = [React.MutableRefObject<T | null>, () => void
 
 const useScrollToElement = <T extends HTMLElement>(): UseScrollToElementReturn<T> => {
   const ref = useRef<T | null>(null)
-  const scrollToRef = (arg?: boolean | ScrollIntoViewOptions | undefined): void => {
+  const scrollToRef = (arg?: boolean | ScrollIntoViewOptions): void => {
     if (ref.current) {
       ref.current.scrollIntoView(arg)
     }
